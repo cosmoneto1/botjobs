13d768ee13691bac1b500c9b58a57c66fa6b9d45
Cosmo Guedes <cosmo.neto@globalhitss.com.br>
2019-02-21 22:25:57 -0300

    adicionado envio de email

--- bot.js
+++ bot.js
@@ -1,16 +1,19 @@
 var indeed = require('./indeed');
 var linkedin = require('./linkedin');
 var arquivo = require('./file');
+var emailJob = require('./email');
+var email = 'teste@hgjh.com';
 
 indeed.consulta().then((lista) => {
     console.log(lista.length);
+    emailJob.send(lista, email);
     arquivo.criarJson('indeed', lista);
     arquivo.criarTxt('indeed', lista);
-
 });
 
 linkedin.consulta().then((list) => {
     console.log(list.length);
+    emailJob.send(list, email);
     arquivo.criarJson('linkedin', list);
     arquivo.criarTxt('linkedin', list);
 });
